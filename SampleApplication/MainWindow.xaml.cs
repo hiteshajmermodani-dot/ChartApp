795ee7e8ed7e21ea1513c3c5bca163cc47d464ec
hiteshajmermodani-dot <hitesh.ajmer.modani@gmail.com>
2026-05-07 12:17:16 +0530

    Add files via upload

---
+++ SampleApplication/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using SampleApplicationChartApp.ViewModels;
+
+namespace SampleApplicationChartApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var vm = new MainViewModel
+                     {
+                         RequestRefresh = () => ChartControl.RefreshChart()
+                     };
+
+            DataContext = vm;
+
+            // Ensure the chart can receive keyboard focus for axis movement
+            ChartControl.Focus();
+        }
+    }
+}
